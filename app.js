f44687bd018047ef5b3cd36b0d73839ac4ba2cc8
ivan2798 <inge1998@hotmail.com>
2022-09-19 12:02:35 -0500

    templates

---
+++ app.js
@@ -0,0 +1,12 @@
+let nombre = "John";
+let apellido = "Wick";
+
+//concatenar
+let saludo = "Hola mi nombre es:"+ nombre + " "+apellido;
+console.log(saludo);
+
+//inteprolación de variables
+//Template String 
+let saludo2 = `Hola mi nombre es ${nombre} ${apellido}`;
+
+console.log(saludo2);
