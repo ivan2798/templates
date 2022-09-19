7be0fcf8702328c2de00c523fc70a3392362c7c8
ivan2798 <inge1998@hotmail.com>
2022-09-19 12:07:07 -0500

    acento invertido

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ console.log(saludo);
 //inteprolación de variables
 //Template String 
 let saludo2 = `Hola mi nombre es ${nombre} ${apellido}`;
-
 console.log(saludo2);
+
+let ul = `<ul>
+<li>Primavera</li>
+<li>Verano</li>
+<li>Otoño</li>
+<li>Invierno</li>
+</ul>`;
+
+console.log(ul);
